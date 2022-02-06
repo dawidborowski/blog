de6ed56a9f85885b38bae06e6c7c14ea10b576f6
dawidborowski <dawid.borowski@vybbe.com>
2022-02-06 18:27:06 +0100

    Final version

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+    get 'home/index'
+
+    resources :articles do resources :comments
+    end
+  
+    root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
